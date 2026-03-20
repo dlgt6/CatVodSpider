965ade55a6faa25276b77998608a28ba358a4294
dlgt6 <157378128+dlgt6@users.noreply.github.com>
2026-03-20 18:36:09 +0800

    Update Jinli.java

--- app/src/main/java/com/github/catvod/spider/Jinli.java
+++ app/src/main/java/com/github/catvod/spider/Jinli.java
@@ -5,6 +5,7 @@ import com.github.catvod.bean.Class;
 import com.github.catvod.bean.Result;
 import com.github.catvod.bean.Vod;
 import com.github.catvod.crawler.Spider;
+import com.github.catvod.crawler.SpiderDebug;
 import com.github.catvod.net.OkHttp;
 import org.json.JSONArray;
 import org.json.JSONObject;
